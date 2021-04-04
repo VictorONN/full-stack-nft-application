c70e1f0868be40e1a8c086dc001028608563a260
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-04-04 09:21:37 +0300

    sc

---
+++ contracts/NFT.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    uint256 public nextTokenId;
+    address public admin;
+
+    constructor() ERC721("My NFT", "NFT") {
+        admin = msg.sender;
+    }
+
+    function mint(address to) external {
+        require(msg.sender == admin, "only admin");
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://shrouded-ridge-89243.herokuapp.com/";
+    }
+}
