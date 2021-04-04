7bc8b46b601b81ee75a019bb4b79ea5dd3241992
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-04-04 12:23:09 +0300

    frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,41 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require('@nomiclabs/hardhat-waffle')
+// module.exports = {
+//   solidity: "0.8.0",
+// };
+
 module.exports = {
-  solidity: "0.8.0",
-};
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/87a7ae81404e427a8057a9b06ed26da1',
+      accounts: {mnemonic: ''}
+    } 
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./frontend/src/artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ""
+  }
+}
